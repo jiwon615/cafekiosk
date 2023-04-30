b5b6dfbff8f1285564096961b50b84bb991bb7ea
jiwon615 <seg615@naver.com>
2023-04-30 22:05:37 +0900

    test: @DisplayName 추가

--- src/test/java/com/study/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/com/study/cafekiosk/unit/CafeKioskTest.java
@@ -3,16 +3,17 @@ package com.study.cafekiosk.unit;
 import com.study.cafekiosk.unit.beverages.Americano;
 import com.study.cafekiosk.unit.beverages.Latte;
 import com.study.cafekiosk.unit.order.Order;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CafeKioskTest {
 
+    @DisplayName("음료 1개 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -72,8 +73,10 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -81,7 +84,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
 
