147d2e0b29d1ec9be49d681dce06ed7057cb6dec
jiwon615 <seg615@naver.com>
2023-05-01 11:34:47 +0900

    test: ProductRepository list test

--- src/main/java/com/study/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/com/study/cafekiosk/spring/domain/product/Product.java
@@ -2,6 +2,7 @@ package com.study.cafekiosk.spring.domain.product;
 
 import com.study.cafekiosk.spring.domain.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,4 +30,13 @@ public class Product extends BaseEntity {
     private String name;
 
     private int price;
+
+    @Builder
+    private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
