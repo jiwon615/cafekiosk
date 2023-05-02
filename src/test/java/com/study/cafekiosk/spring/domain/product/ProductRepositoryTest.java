fbbe2cd845bafaee5cc13a54b0570d143dddcec3
jiwon615 <seg615@naver.com>
2023-05-02 20:27:39 +0900

    test: 중복된 상품번호로 주문 가능하도록 테스트 추가

--- src/test/java/com/study/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/study/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -4,16 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static com.study.cafekiosk.spring.domain.product.ProductSellingStatus.*;
-import static com.study.cafekiosk.spring.domain.product.ProductType.*;
+import static com.study.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 //@SpringBootTest // 스프링 서버 띄움
