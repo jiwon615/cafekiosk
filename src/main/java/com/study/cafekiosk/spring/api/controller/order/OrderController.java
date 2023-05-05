428dfcbf5de1ac6cdde5d1d8a23b9f2d7226c9a0
jiwon615 <seg615@naver.com>
2023-05-05 17:20:13 +0900

    test: OrderControllerTest

--- src/main/java/com/study/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/com/study/cafekiosk/spring/api/controller/order/OrderController.java
@@ -1,13 +1,16 @@
 package com.study.cafekiosk.spring.api.controller.order;
 
+import com.study.cafekiosk.spring.api.ApiResponse;
 import com.study.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import com.study.cafekiosk.spring.api.service.order.OrderService;
+import com.study.cafekiosk.spring.domain.order.response.OrderResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -17,9 +20,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public ResponseEntity<?> createOrder(@RequestBody OrderCreateRequest request) {
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
         LocalDateTime registeredDateTime = LocalDateTime.now();
-        orderService.createOrder(request, registeredDateTime);
-        return ResponseEntity.ok().build();
+        return ApiResponse.ok(orderService.createOrder(request, registeredDateTime));
     }
 }
