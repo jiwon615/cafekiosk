8f648612dd4e471a616ee5ac5d8e52daca9b1ddc
jiwon615 <seg615@naver.com>
2023-05-07 12:25:39 +0900

    test: Enum 효율적으로 테스트 하기 - @ParameterizedTest

--- src/test/java/com/study/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/com/study/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -3,6 +3,12 @@ package com.study.cafekiosk.spring.domain.product;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,4 +54,41 @@ class ProductTypeTest {
         // then
         assertThat(result).isTrue();
     }
+
+    /**
+     * @ParameterizedTest, @CsvSource 사용하면 좀 더 편리하게 위 1~3 테스트를 한 번에 검증 가능
+     */
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @CsvSource({"HANDMADE,false","BOTTLE,true","BAKERY,true"})
+    @ParameterizedTest
+    void containsStockType4(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    /**
+     * ENUM을 테스트 하는 또다른 효율적인 방법 (테스트해야할 ENUM 개수가 많아지면 아래꺼 사용)
+     */
+    private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @MethodSource("provideProductTypesForCheckingStockType")
+    @ParameterizedTest
+    void containsStockType5(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
 }
